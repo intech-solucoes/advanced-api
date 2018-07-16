68b6674c5414b43e95cf86d70e1ce5198f9a1eed
Rony Moura <ronymmoura@gmail.com>
2018-07-16 18:04:20 -0300

    Ajuste na rota de versão

--- Faceb/Intech.Advanced.FacebApi/Controllers/VersaoController.cs
+++ Faceb/Intech.Advanced.FacebApi/Controllers/VersaoController.cs
@@ -8,14 +8,14 @@ namespace Intech.Advanced.FacebApi.Controllers
 {
     [Route("api/")]
     [ApiController]
-    public class VersaoController : ControllerBase
+    public class VersaoController : Controller
     {
         public IActionResult Get()
         {
             try
             {
                 var version = Assembly.GetExecutingAssembly().GetName();
-                return Ok(new { Versao = version.Version.ToString(3) });
+                return Json(version.Version.ToString(3));
             }
             catch (Exception ex)
             {
